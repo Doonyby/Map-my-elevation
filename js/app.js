4a1ee580226b20ff05c66d9388817e2716ee5dce
Dan Riding <doonyby@gmail.com>
2016-06-16 08:28:38 -0600

    retrieved latLng

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+	$('input[name="button"]').click(function(e) {
+		e.preventDefault();
+		var start = $('input[name="start"]').val()
+		var finish = $('input[name="finish"]').val()
+			
+		geocodeInfo(start);
+		geocodeInfo(finish);
+		
+	});
 
+	var geocodeInfo = function(start, finish) {
+		var params = {
+			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
+			inFormat: 'kvp',
+			outFormat: 'json',
+			maxResults: 1,
+			location: start,
+		};
+		url = 'http://www.mapquestapi.com/geocoding/v1/address';
+
+		$.getJSON(url, params, function(data) {
+			getLatLng(data.results)
+		});
+	}
+	
+	function getLatLng(result) {
+		lattitude = result[0].locations[0].latLng.lat;
+		longitude = result[0].locations[0].latLng.lng;
+		return lattitude + " " + longitude;
+	}
 
 });
+
