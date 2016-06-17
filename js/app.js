87c4ae9d6c7645210867e54aa22ffdf0650f6a2f
Dan Riding <doonyby@gmail.com>
2016-06-17 10:18:47 -0600

    start and finish geocode functions

--- js/app.js
+++ js/app.js
@@ -4,30 +4,63 @@ $(document).ready(function () {
 		var start = $('input[name="start"]').val()
 		var finish = $('input[name="finish"]').val()
 			
-		geocodeInfo(start);
-		geocodeInfo(finish);
+		getStartGeocode(start);
+		getFinishGeocode(finish);
 		
 	});
+	var startLatLng = "";
+	var finishLatLng = "";
+	
+	function getElevationInfo() {
+		var coordinates = startLatLng + "," + finishLatLng;
+		console.log(coordinates);
+		// var params = {
+		// 	key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
+		// 	inFormat: 'kvp',
+		// 	shapeFormat: 'raw',
+		// 	width: 425,
+		// 	height: 350,
+		// 	latLngCollection: latLng
+		// };
+		// url = 'http://open.mapquestapi.com/elevation/v1/chart';
 
-	var geocodeInfo = function(start, finish) {
+		// $.getJSON(url, params, function(data) {
+		// 	console.log(data);
+		// });
+	}
+
+
+	var getStartGeocode = function(start) {
 		var params = {
 			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
 			inFormat: 'kvp',
 			outFormat: 'json',
 			maxResults: 1,
-			location: start,
+			location: start
 		};
 		url = 'http://www.mapquestapi.com/geocoding/v1/address';
-
 		$.getJSON(url, params, function(data) {
-			getLatLng(data.results)
+			var lattitude = data.results[0].locations[0].latLng.lat;
+			var longitude = data.results[0].locations[0].latLng.lng;
+			startLatLng += lattitude + "," + longitude;
 		});
 	}
-	
-	function getLatLng(result) {
-		lattitude = result[0].locations[0].latLng.lat;
-		longitude = result[0].locations[0].latLng.lng;
-		return lattitude + " " + longitude;
+
+	var getFinishGeocode = function(finish) {
+		var params = {
+			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
+			inFormat: 'kvp',
+			outFormat: 'json',
+			maxResults: 1,
+			location: finish
+		};
+		url = 'http://www.mapquestapi.com/geocoding/v1/address';
+		$.getJSON(url, params, function(data) {
+			var lattitude = data.results[0].locations[0].latLng.lat;
+			var longitude = data.results[0].locations[0].latLng.lng;
+			finishLatLng += lattitude + "," + longitude;
+			getElevationInfo();
+		});
 	}
 
 });
