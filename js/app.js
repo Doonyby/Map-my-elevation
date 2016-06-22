3230fd9ad94b6c96fba610269220203da84f2ffc
Dan Riding <doonyby@gmail.com>
2016-06-22 12:49:59 -0600

    replaced line elevation chart

--- js/app.js
+++ js/app.js
@@ -7,58 +7,9 @@ $(document).ready(function () {
 		getGeocodeInfo(start, finish);
 		
 	});
+
 	var startLatLng = "";
 	var finishLatLng = "";
-	
-	function getElevationChartInfo() {
-		var coordinates = startLatLng + "," + finishLatLng;
-		$('#chart').html('<img style="-webkit-user-select: none; cursor: zoom-in;" src="http://open.mapquestapi.com/elevation/v1/chart?key=DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf&amp;inFormat=kvp&amp;shapeFormat=raw&amp;unit=f&amp;width=425&amp;height=350&amp;latLngCollection=' + coordinates + '" width="398" height="328">');
-	}
-
-	function getElevationTableInfo() {
-		var coordinates = startLatLng + "," + finishLatLng;
-		var params = {
-			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
-			inFormat: 'kvp',
-			shapeFormat: 'raw',
-			unit: "f",
-			width: 425,
-			height: 350,
-			latLngCollection: coordinates
-		};
-		url = 'http://open.mapquestapi.com/elevation/v1/profile';
-
-		$.getJSON(url, params, function(data) {
-			$('#startElev').text('Starting Elevation: ' + data.elevationProfile[0].height + ' ft. above sea level.');
-			$('#endElev').text('Ending Elevation: ' + data.elevationProfile[1].height + ' ft. above sea level.');
-		});
-	}
-
-	function getRouteMap() {
-		var coordinates = startLatLng + "," + finishLatLng;
-		var params = {
-			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
-			from: startLatLng,
-			to: finishLatLng,
-			inFormat: 'kvp',
-			shapeFormat: 'raw',
-			unit: "m",
-			routeType: "fastest",
-			narrativeType: 'none',
-			mapwidth: 425,
-			mapheight: 350,
-		};
-		url = 'http://open.mapquestapi.com/directions/v2/route';
-
-		$.getJSON(url, params, function(data) {
-			$('#distance').text('Total Distance: ' + data.route.distance + ' miles');
-			var session = data.route.sessionId;
-			console.log(data);
-			$('#map').html('<img src="http://open.mapquestapi.com/directions/v2/routeshape?key=DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf&callback=renderShapeResults&json={sessionId:' + session + ', mapState:{width: 320, height:240, scale:866685, center:{lat:40.5312875, lng:-111.792457}}}">')
-			//$('#chart').html('<img style="-webkit-user-select: none; cursor: zoom-in;" src="http://open.mapquestapi.com/elevation/v1/chart?key=DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf&amp;inFormat=kvp&amp;shapeFormat=raw&amp;unit=f&amp;width=425&amp;height=350&amp;sessionId=' + session + '" width="398" height="328">');
-
-		});
-	}
 
 	var getGeocodeInfo = function(start, finish) {
 		var startParams = {
@@ -110,9 +61,53 @@ $(document).ready(function () {
 		        alert("No finish data!");
 		    }
 		});
-
-		
+	}
+	
+	function getElevationChartInfo(session) {
+		var coordinates = startLatLng + "," + finishLatLng;
+		$('#chart').html('<h2>Elevation Chart of Route</h2><br><br><img style="-webkit-user-select: none; cursor: zoom-in;" src="http://open.mapquestapi.com/elevation/v1/chart?key=DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf&amp;inFormat=kvp&amp;shapeFormat=raw&amp;unit=f&amp;width=425&amp;height=350&amp;latLngCollection=' + coordinates + '" width="398" height="328">');
 	}
 
+	function getElevationTableInfo() {
+		var coordinates = startLatLng + "," + finishLatLng;
+		var params = {
+			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
+			inFormat: 'kvp',
+			shapeFormat: 'raw',
+			unit: "f",
+			width: 425,
+			height: 350,
+			latLngCollection: coordinates
+		};
+		url = 'http://open.mapquestapi.com/elevation/v1/profile';
+
+		$.getJSON(url, params, function(data) {
+			$('#startElev').text('Starting Elevation: ' + data.elevationProfile[0].height + ' ft. above sea level.');
+			$('#endElev').text('Ending Elevation: ' + data.elevationProfile[1].height + ' ft. above sea level.');
+		});
+	}
+
+	function getRouteMap() {
+		var coordinates = startLatLng + "," + finishLatLng;
+		var params = {
+			key: 'DkTAlgpIf3NGiuI1P7ZmHIC280KSgwVf',
+			from: startLatLng,
+			to: finishLatLng,
+			inFormat: 'kvp',
+			shapeFormat: 'raw',
+			unit: "m",
+			routeType: "fastest",
+			narrativeType: 'none',
+			mapwidth: 425,
+			mapheight: 350,
+		};
+		url = 'http://open.mapquestapi.com/directions/v2/route';
+
+		$.getJSON(url, params, function(data) {
+			$('#distance').text('Total Distance: ' + data.route.distance + ' miles');
+			var session = data.route.sessionId;
+			console.log(data);
+		});
+	}
 });
 
